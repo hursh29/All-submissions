cada54cdbc751ec24387d22a8d8f6de8cb67d011
hursh29 <hurshs0609@gmail.com>
2020-03-28 19:34:55 +0530

    final template

--- submit.cpp
+++ submit.cpp
@@ -21,42 +21,7 @@ using namespace __gnu_pbds;
 typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set ; 
 const int N = 2e5+7;
 int tests(){ 
-  ll i ;
-  ll x,y ;
-  cin >> x >> y ;
-  ll a,b,c ; 
-  cin >> a >> b >> c ; 
-  vector <ll> p(a),q(b),r(c);
-  vector <ll> t ; 
-  map <ll,ll> m;
-  for(auto &x :p)
-    cin >> x ; 
-  for(auto &x :q)
-    cin >> x;
-  for(auto &x :r)
-    cin >> x,m[x]++ ;
-  sort(all(p));
-  sort(all(q)); 
-  sort(all(r));
-  reverse(all(p));
-  reverse(all(q)); 
   
-  forn(i,0,x)
-    t.pb(p[i]);
-
-  forn(i,0,y)
-    t.pb(q[i]);
-  sort(all(t));
-  // reverse(all(t));  
-  forn(i,0,x+y)
-    if(!r.empty() && t[i] < r.back()){
-      t[i] = r.back();
-      r.pop_back() ;
-    }  
-    ll sum  = 0 ; 
-  forn(i,0,x+y)
-    sum += (ll)t[i];  
-  cout << sum << '\n' ; 
   return 0 ;
 } 
 int32_t main(){
